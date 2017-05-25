adf776e3586c3ab90a2f0ae8094f1afc9df1670b
AutumnCharnesky <autumn.breeze.c18@gmail.com>
2017-05-25 10:26:57 -0400

    js ang

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
 var app = angular.module("ticketKiosk", ["ui.bootstrap"]);
+
+app.controller('kioskController', function($scope) {
+
+  $scope.master = {name: "", ticketNum: 1};
+  $scope.purchase = function() {
+
+    $scope.order = angular.copy($scope.master);
+  };
+
+$scope.purchaser = [];
+// $scope.order.ticketNum = 1;
+
+  $scope.purchase = function(order) {
+    console.log(order);
+    if(order.ticketNum === 1) {
+    $scope.purchaser.unshift(order.name +" purchased " + order.ticketNum + " ticket.")
+    console.log($scope.purchaser);
+  } else {
+    $scope.purchaser.unshift(order.name +" purchased " + order.ticketNum + " tickets.")
+    console.log($scope.purchaser);
+  }
+    $scope.order.ticketNum = 1;
+    $scope.order.name = '';
+  }
+
+});
+
+
+// $scope.purchase();
+//
+
+//
+// $scope.newsFeed = function() {
+//   console.log(newsFeed);
+//   $scope.purchaser.push({name: newsFeed.nsme});
+//   console.log($scope.purchaser)
+// }
